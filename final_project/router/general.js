a347c06142b83a1f904648f343aac310ef129d8f
Elliott Chkayben <eliott.chkayben@gmail.com>
2025-03-31 22:26:30 -0400

    Tasks 10-13 using promise and axios

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -3,6 +3,7 @@ let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
+const axios = require('axios');
 
 
 public_users.post("/register", (req,res) => {
@@ -23,48 +24,128 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-    res.send(JSON.stringify(books, null, 4))
+  // res.send(JSON.stringify(books, null, 4))
+  // Promise-based approach
+    const getAllBooks = () => {
+      return new Promise((resolve, reject) => {
+        try {
+          resolve(books)
+        } catch(error) {
+          reject(error)
+        }
+    })
+    }
+
+    getAllBooks()
+    .then(books => res.json(books))
+    .catch(error => res.status(500).json({ message: "Error fetching books", error: error.message }));
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  const isbn = req.params.isbn;
-  const book = books[isbn]
-  if(book) {
-    res.send(JSON.stringify(book, null, 4))
-  } else {
+public_users.get('/isbn/:isbn', async function (req, res) {
+  // const isbn = req.params.isbn;
+  // const book = books[isbn]
+  // if(book) {
+  //   res.send(JSON.stringify(book, null, 4))
+  // } else {
+  //   res.status(404).json({ message: "Book not found" })
+  // }
+  // using async/await
+  try {
+    const isbn = req.params.isbn
+
+    // Simulating an API call with axios
+    const getBookByISBN = async(isbn) => {
+      // In a real scenario, this would be an actual API endpoint
+      // return await axios.get(`https://api.books.com/books/${isbn}`);
+
+      const book = books[isbn]
+      if(!book) {
+        throw new Error('Book not found')
+      } else {
+        return { data: book }
+      }
+    }
+
+    const response = await getBookByISBN(isbn)
+    res.json(response.data)
+  } catch(error) {
     res.status(404).json({ message: "Book not found" })
   }
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  const author = decodeURIComponent(req.params.author)
-  const normalizedAuthor= author.normalize('NFC')
-  const bookByAuthor = Object.values(books).filter(book => {
-    const normalizedBookAuthor = book.author.normalize('NFC')
-    return normalizedBookAuthor.toLowerCase() === normalizedAuthor.toLowerCase()
-  })
-  if(bookByAuthor.length > 0) {
-    res.send(JSON.stringify(bookByAuthor, null, 4))
-  } else {
-    res.status(404).json({ message: "Book not found" })
-  }
+
+    // Promise-based approach
+    const getBooksByAuthor = () => {
+      return new Promise((resolve, reject) => {
+        try {
+          const author = decodeURIComponent(req.params.author)
+          const normalizedAuthor= author.normalize('NFC')
+            const bookBysAuthor = Object.values(books).filter(book => {
+              const normalizedBookAuthor = book.author.normalize('NFC')
+              return normalizedBookAuthor.toLowerCase() === normalizedAuthor.toLowerCase()
+            })
+          resolve(bookBysAuthor)
+        } catch(error) {
+          reject(error)
+        }
+    })
+    }
+
+    getBooksByAuthor()
+    .then(books => res.json(books))
+    .catch(error => res.status(500).json({ message: "Book not found", error: error.message }));
+
+  // const author = decodeURIComponent(req.params.author)
+  // const normalizedAuthor= author.normalize('NFC')
+  // const bookByAuthor = Object.values(books).filter(book => {
+  //   const normalizedBookAuthor = book.author.normalize('NFC')
+  //   return normalizedBookAuthor.toLowerCase() === normalizedAuthor.toLowerCase()
+  // })
+  // if(bookByAuthor.length > 0) {
+  //   res.send(JSON.stringify(bookByAuthor, null, 4))
+  // } else {
+  //   res.status(404).json({ message: "Book not found" })
+  // }
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  const title = decodeURIComponent(req.params.title)
-  const normalizedTitle = title.normalize('NFC')
-  const bookByTitle = Object.values(books).filter(book => {
-    const normalizedBookTitle = book.title.normalize('NFC')
-    return normalizedBookTitle.toLowerCase() === normalizedTitle.toLowerCase()
+
+  // Promise-based approach
+  const getBooksByAuthor = () => {
+    return new Promise((resolve, reject) => {
+      try {
+        const title = decodeURIComponent(req.params.title)
+        const normalizedTitle = title.normalize('NFC')
+        const bookByTitle = Object.values(books).filter(book => {
+          const normalizedBookTitle = book.title.normalize('NFC')
+          return normalizedBookTitle.toLowerCase() === normalizedTitle.toLowerCase()
+        })
+        resolve(bookByTitle)
+      } catch(error) {
+        reject(error)
+      }
   })
-  if(bookByTitle.length > 0) {
-    res.send(JSON.stringify(bookByTitle, null, 4))
-  } else {
-    res.status(404).json({ message: "Book not found" })
   }
+
+  getBooksByAuthor()
+  .then(books => res.json(books))
+  .catch(error => res.status(500).json({ message: "Book not found", error: error.message }));
+
+  // const title = decodeURIComponent(req.params.title)
+  // const normalizedTitle = title.normalize('NFC')
+  // const bookByTitle = Object.values(books).filter(book => {
+  //   const normalizedBookTitle = book.title.normalize('NFC')
+  //   return normalizedBookTitle.toLowerCase() === normalizedTitle.toLowerCase()
+  // })
+  // if(bookByTitle.length > 0) {
+  //   res.send(JSON.stringify(bookByTitle, null, 4))
+  // } else {
+  //   res.status(404).json({ message: "Book not found" })
+  // }
 });
 
 //  Get book review
